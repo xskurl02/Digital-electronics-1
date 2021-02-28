17d4ec3cae30d01bc1cd388dcf1beed49ac8e14f
Tomas_Skurla <xskurl02@vutbr.cz>
2021-02-28 20:15:27 +0100

    last upload of images

---
+++ Labs/03-Vivado/Solutions/project_1/project_1.srcs/sources_1/new/foo2.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 28.02.2021 20:14:14
+-- Design Name: 
+-- Module Name: foo2 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity foo2 is
+--  Port ( );
+end foo2;
+
+architecture Behavioral of foo2 is
+
+begin
+
+
+end Behavioral;
